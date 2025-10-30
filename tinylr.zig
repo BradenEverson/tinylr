9083da02a61a216ce511db722b9a315e0d74f165
BradenEverson <bradeneverson@gmail.com>
2025-10-30 05:16:08 -0500

    Inferences work!

--- tinylr.zig
+++ tinylr.zig
@@ -5,32 +5,59 @@ const std = @import("std");
 pub fn LrModel(comptime T: type, comptime features: comptime_int) type {
     return struct {
         w: [features]T,
-        w0: T,
+        bias: T,
 
         const Self = @This();
 
-        pub fn init(w: [features]T, w0: T) Self {
+        pub fn init(w: [features]T, bias: T) Self {
             return Self{
                 .w = w,
-                .w0 = w0,
+                .bias = bias,
             };
+        }
+
+        fn doublePercision() type {
+            const info = @typeInfo(T);
+
+            return @Type(.{
+                .int = .{
+                    .signedness = info.int.signedness,
+                    .bits = info.int.bits * 2,
+                },
+            });
         }
 
         /// Performs an optimized version of logistic regression w/ threshold = 0.5
         /// This allows us to simplify the expression, if w dot (x - w0) >= 0, class 1 (true)
         /// Else, class 0 (false)
         pub fn infer(self: *const Self, x: [features]T) bool {
-            _ = self;
-            _ = x;
-            false;
+            var result: doublePercision() = 0;
+            for (0..features) |i| {
+                result += self.w[i] * x[i];
+            }
+
+            return result + self.bias >= 0;
         }
     };
 }
 
 test "construction" {
-    const Model = LrModel(u8, 2);
+    const Model = LrModel(i8, 2);
     const m: Model = Model.init(.{ 1, 2 }, 3);
 
     try std.testing.expectEqual(m.w, .{ 1, 2 });
-    try std.testing.expectEqual(m.w0, 3);
+    try std.testing.expectEqual(m.bias, 3);
+}
+
+test "inference" {
+    const Model = LrModel(i8, 2);
+    const m: Model = Model.init(.{ 1, 2 }, 3);
+
+    // Should be above hyperplane (class 1)
+    const x1 = .{ 4, 5 };
+    // Should be below hyperplane (class 0)
+    const x2 = .{ -2, -2 };
+
+    try std.testing.expect(m.infer(x1));
+    try std.testing.expect(!m.infer(x2));
 }
